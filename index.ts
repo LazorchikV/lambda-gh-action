b7851f221de0bd3d459d23bbd20bd276c4733ff6
Vitaly Lazorchik <53997449+LazorchikV@users.noreply.github.com>
2021-08-13 22:47:30 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -2,12 +2,9 @@ import { APIGatewayProxyEvent, APIGatewayProxyResultV2, Handler } from 'aws-lamb
 import * as _ from 'lodash';
 
 export const handler: Handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResultV2> => {
-  //const max = 999;
- // const val = _.random(max);
   const response = {
     statusCode: 200,
     body: "Hello World!"
-    //`The random value (max ${max}) is: ${val}`,
   };
   return response;
 };
